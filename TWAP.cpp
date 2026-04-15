0d391ac26bc56039337d3d51eb85c5ccb6bff1c7
CoolFixCoder <113441129+CoolFixCoder@users.noreply.github.com>
2026-04-15 21:13:28 +0530

    Create TWAP.cpp
    TWAP= Optimal Execution Technique

---
+++ TWAP.cpp
@@ -0,0 +1,9 @@
+/*
+It is an algorithmic trading strategy that executes large orders by breaking them into smaller equal-sized slices,
+spreading evenly over a specific time interval. 
+It minimizes market impact by avoiding single large transaction, makin git ideal for illiquid markets to prevent
+significant price disruption
+Splits order into equal slices, executed at regular intervals
+
+It is one of the Optimal Execution Technique
+*/
