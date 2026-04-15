e1b6892a87d22d0bbbac755ca3794277846c63ad
CoolFixCoder <113441129+CoolFixCoder@users.noreply.github.com>
2026-04-15 21:23:29 +0530

    Create PercentOfVolume.cpp
    Percent of volume = Optimal Execution Technique

---
+++ PercentOfVolume.cpp
@@ -0,0 +1,7 @@
+/*
+It is a trading strategy that aims to execution an order as a specified percentage of the market total volume over a given period.
+POV controls the rate at which trades are executed relative to the market's trading volume.
+POV is defined as the ratio of the trader's order size to the total market volume over a specified time frame, expresses as a percentage.
+For instance, if a trader sets POV rate of 20%, the traders intends to execute their orders such that it constitutes no more than 20% of the market's total 
+    volume in each time slice, thus reducing market impacts.
+*/
